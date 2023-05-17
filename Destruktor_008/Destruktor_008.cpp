25004170098c5865930e48d40b7caa42b70f52a4
DzarZaki <dzarzaki2@gmail.com>
2023-05-17 18:09:00 +0700

    menambahkan" atau "meng-commit" penghapusan memori dinamis yang dilakukan dengan menggunakan operator delete terhadap pointer ptrBelajarcpp dan penutup fungsi angka::~angka() ke dalam repositori.

--- Destruktor_008/Destruktor_008.cpp
+++ Destruktor_008/Destruktor_008.cpp
@@ -29,4 +29,5 @@ void angka::cetakData() {
 int main() {
 	angka belajarcpp(3); // Constructor Dipanggil
 	angka* ptrBelajarcpp = new angka(5); // Constructor Dipanggil
+	delete ptrBelajarcpp; // Destructor Dipanggil 
 }
